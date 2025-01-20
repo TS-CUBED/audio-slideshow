dec0e75e666d79d46ed7200773828e82d6f08bd4
kapsner <lorenz.kapsner@web.de>
2025-01-20 16:40:26 +0100

    first commit

---
+++ update_plugin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd _extensions/audio-slideshow
+wget https://raw.githubusercontent.com/rajgoel/reveal.js-plugins/refs/heads/master/audio-slideshow/plugin.js
+wget https://raw.githubusercontent.com/rajgoel/reveal.js-plugins/refs/heads/master/audio-slideshow/recorder.js
+wget https://raw.githubusercontent.com/rajgoel/reveal.js-plugins/refs/heads/master/audio-slideshow/RecordRTC.js
