3eb7b65d3045885dfc2c9c5820805c01078a491f
Torsten Schenkel <ts-cubed@t-cubed.org.uk>
2025-11-24 16:51:53 +0000

    Change hotkey for recording to N
    Change hotkey for recording narrative to "N" to avoid conflict
    with the notes canvas hotkey "C" of the chalkboard plugin.

--- _extensions/audio-slideshow/recorder.js
+++ _extensions/audio-slideshow/recorder.js
@@ -298,8 +298,8 @@ console.warn(blob.type);
 
 
 const initAudioRecorder = function(Reveal){
-  Reveal.addKeyBinding( { keyCode: 67, key: 'C', description: 'Toggle recording' }, function() { Recorder.toggleRecording(); } );
-  Reveal.addKeyBinding( { keyCode: 90, key: 'Z', description: 'Download recordings' }, function() { Recorder.downloadZip(); } );
+  Reveal.addKeyBinding( { keyCode: 78, key: 'N', description: 'Toggle narrative recording' }, function() { Recorder.toggleRecording(); } );
+  Reveal.addKeyBinding( { keyCode: 90, key: 'Z', description: 'Download narrative recordings' }, function() { Recorder.downloadZip(); } );
   Reveal.addKeyBinding( { keyCode: 84, key: 'T', description: 'Fetch text-to-speech audio files' }, function() { Recorder.fetchTTS(); } );
 
 	Reveal.addEventListener( 'fragmentshown', function( event ) {
